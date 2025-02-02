0945f06f09635f0540c60a424f42feee28870d4c
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-02 22:49:33 +0200

    Solution v4

--- app/test_main.py
+++ app/test_main.py
@@ -5,14 +5,24 @@ from app.main import outdated_products
 
 
 def test_outdated_products() -> None:
-    test_products = [
-        {"name": "Apple", "expiration_date": datetime.date(2021, 1, 30)},
-        {"name": "Banana", "expiration_date": datetime.date(2025, 4, 30)},
-        {"name": "Orange", "expiration_date": datetime.date(2025, 12, 30)},
-    ]
+    test_products = [{
+        "name": "salmon",
+        "expiration_date": datetime.date(2022, 2, 10),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 2, 5),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 2, 1),
+        "price": 160
+    }]
 
     fake_today = datetime.date(2022, 2, 5)
 
     with patch("app.main.datetime") as mock_datetime:
         mock_datetime.date.today.return_value = fake_today
-        assert outdated_products(test_products) == ["Apple"]
+        assert outdated_products(test_products) == ["duck"]
