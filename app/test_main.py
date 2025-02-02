c9e12a6d0c3ca3a2adff5dd67a1bcd347822c848
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-02 22:27:54 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from datetime import date
+import pytest
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "test_outdated_products, expected_products",
+    [
+        (
+            [
+                {"name": "Apple", "expiration_date": date(2025, 1, 30)},
+                {"name": "Banana", "expiration_date": date(2025, 4, 30)},
+                {"name": "Orange", "expiration_date": date(2025, 12, 30)},
+            ],
+            ["Apple"]
+        ),
+        (
+            [],
+            []
+        )
+    ]
+)
+def test_outdated_products(test_outdated_products, expected_products):
+    assert outdated_products(test_outdated_products) == expected_products
