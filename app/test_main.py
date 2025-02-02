7095da3afd47eef10ec872bb0f1c81b7b85d004a
Tymofii Khlistik <tima0323199@gmail.com>
2025-02-02 22:51:59 +0200

    Solution v4

--- app/test_main.py
+++ app/test_main.py
@@ -9,13 +9,11 @@ def test_outdated_products() -> None:
         "name": "salmon",
         "expiration_date": datetime.date(2022, 2, 10),
         "price": 600
-    },
-    {
+    }, {
         "name": "chicken",
         "expiration_date": datetime.date(2022, 2, 5),
         "price": 120
-    },
-    {
+    }, {
         "name": "duck",
         "expiration_date": datetime.date(2022, 2, 1),
         "price": 160
